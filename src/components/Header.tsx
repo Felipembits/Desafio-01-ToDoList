b1ed7ab2fe53fc8558052a1053edf65b3582782c
Felipe Martins Bittencourt <felipebittencourt@id.uff.br>
2024-02-02 20:58:30 -0300

    feat: add Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,12 @@
 import styles from './Header.module.css';
+import rocket from '../assets/rocket.svg'
+
 
 export function Header() {
   return (
     <header className={styles.header}>
-      <h1>Header</h1>
+      <img src={rocket} alt="Logo de um foguete em forma cartoonesca nas cores azul e púrpura" />
+      <h1 className={styles.appName}>todo</h1>
     </header>
   );
 }
