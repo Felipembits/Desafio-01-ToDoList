4e79ecf1cdfcbb3f16cbae14c2c68a503278a263
Felipe Martins Bittencourt <felipebittencourt@id.uff.br>
2024-02-03 12:54:23 -0300

    feat: novas tarefas funcionando

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,23 @@ import { NewTask } from "./components/NewTask";
 import { TaskBoard } from "./components/TaskBoard";
 
 function App() {
-
   
   const [tarefas, setTarefas] = useState([
-    { title: "Teste de task" },
-    { title: "123123123" },
+    { title:"Teste de task"},
+    { title:"teste"},
   ]);
+
+  function handleCreateNewTask(newTask: any) {
+    setTarefas([...tarefas, newTask])
+    console.log(tarefas)
+  }
 
   return (
     <>
       <Header />
       <section className='appBody'>
         <div className="appContainer">
-          <NewTask/>
+          <NewTask onCreateNewTask={handleCreateNewTask}/>
           <TaskBoard tarefas={tarefas} />
         </div>
       </section>
--- src/components/NewTask.tsx
+++ src/components/NewTask.tsx
@@ -1,13 +1,33 @@
 import { PlusCircle } from "phosphor-react";
 import styles from "./NewTask.module.css";
+import { ChangeEvent, FormEvent, useState } from "react";
 
-export function NewTask() {
+interface NewTaskProps {
+  onCreateNewTask: (newTask: any) => void;
+}
+
+export function NewTask({onCreateNewTask} : NewTaskProps) {
+
+  const [newTaskText, setNewTaskText] = useState("")
+
+  function handleCreateNewTask(event: FormEvent){
+    event.preventDefault();
+    const newTask = {
+      title: newTaskText
+    }
+    onCreateNewTask(newTask)
+  }  
+
+  function handleNewTaskText(event: ChangeEvent<HTMLInputElement>) {
+    setNewTaskText(event.target.value)
+  }
   return (
-    <form className={styles.newTaskForm}>
+    <form className={styles.newTaskForm} onSubmit={handleCreateNewTask}>
       <input
         type="text"
         className={styles.newTaskInput}
         placeholder="Adicionar uma nova tarefa"
+        onChange={handleNewTaskText}
       />
       <button className={styles.newTaskBtn}>
         Criar <PlusCircle size={16}/>
