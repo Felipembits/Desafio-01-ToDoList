5f74a867f1ec8a9a758c9d577b07ffe135abc9d6
Felipe Martins Bittencourt <felipebittencourt@id.uff.br>
2024-02-03 10:13:27 -0300

    feat: passando a tarefa como parâmetro

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,21 +1,22 @@
 import styles from "./Task.module.css";
 import { Circle, Trash } from "phosphor-react";
 
-export function Task() {
+interface TaskProps {
+  title: string;
+}
+
+export function Task(tarefa: TaskProps) {
   return (
     <div className={styles.taskContainer}>
       <div className={styles.taskInfo}>
         <button className={styles.checkCircle}><Circle size={24} weight="bold" /></button>
         <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae
-          non laborum odit error suscipit excepturi repellendus alias ratione,
-          pariatur esse provident consectetur aliquam sit, libero, aut
-          dignissimos impedit nemo a!
+          {tarefa.title}
         </p>
-        <button className={styles.trash}>
+      </div>
+      <button className={styles.trash}>
           <Trash size={20} />
         </button>
-      </div>
     </div>
   );
 }
--- src/components/TaskBoard.tsx
+++ src/components/TaskBoard.tsx
@@ -1,24 +1,35 @@
-import { NoTasksHere } from './NoTasksHere'
-import { Task } from './Task'
-import styles from './TaskBoard.module.css'
+import { useState } from "react";
+import { NoTasksHere } from "./NoTasksHere";
+import { Task } from "./Task";
+import styles from "./TaskBoard.module.css";
 
 export function TaskBoard() {
-    return (
-        <section className={styles.taskBoardArea}>
-            <header className={styles.taskBoardHeader}>
-                <div className={styles.counterTitle}>
-                    <h2 className={styles.createdTasks}>Tarefas criadas</h2>
-                    <span className={styles.counterBall}><p>0</p></span>
-                </div>
-                <div className={styles.counterTitle}>
-                    <h2 className={styles.concludedTasks}>Concluídas</h2>
-                    <span className={styles.counterBall}><p>0</p></span>
-                </div>
-            </header>
-            <div className={styles.taskBoardBlock}>
-                <NoTasksHere/>
-                <Task/>
-            </div>
-        </section>
-    )
+  const [tarefas, setTarefas] = useState([
+    { title: "Teste de task" },
+    { title: "123123123" },
+  ]);
+  return (
+    <section className={styles.taskBoardArea}>
+      <header className={styles.taskBoardHeader}>
+        <div className={styles.counterTitle}>
+          <h2 className={styles.createdTasks}>Tarefas criadas</h2>
+          <span className={styles.counterBall}>
+            <p>0</p>
+          </span>
+        </div>
+        <div className={styles.counterTitle}>
+          <h2 className={styles.concludedTasks}>Concluídas</h2>
+          <span className={styles.counterBall}>
+            <p>0</p>
+          </span>
+        </div>
+      </header>
+    <div className={styles.taskBoardBlock}>
+        <NoTasksHere />
+        {tarefas.map(tarefa => {
+            return <Task key={tarefa.title} {...tarefa} />;
+        })}
+    </div>
+    </section>
+  );
 }
