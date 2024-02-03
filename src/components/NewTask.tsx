8066a4cafe2a0d3d79061b41eec71ccab8714108
Felipe Martins Bittencourt <felipebittencourt@id.uff.br>
2024-02-02 22:42:24 -0300

    feat: add icone

--- src/components/NewTask.tsx
+++ src/components/NewTask.tsx
@@ -1,10 +1,17 @@
-import styles from './NewTask.module.css'
+import { PlusCircle } from "phosphor-react";
+import styles from "./NewTask.module.css";
 
 export function NewTask() {
-    return (
-        <form className={styles.newTaskForm}>
-            <input type="text" className={styles.newTaskInput} placeholder='Adicionar uma nova tarefa'/>
-            <button className={styles.newTaskBtn}>Criar</button>
-        </form>
-    )
+  return (
+    <form className={styles.newTaskForm}>
+      <input
+        type="text"
+        className={styles.newTaskInput}
+        placeholder="Adicionar uma nova tarefa"
+      />
+      <button className={styles.newTaskBtn}>
+        Criar <PlusCircle size={16}/>
+      </button>
+    </form>
+  );
 }
